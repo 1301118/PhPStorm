2dda7bed6abb577862e2643da46bae6fd95f4aaa
1301118 <1301118@rgu.ac.uk>
2016-10-03 15:24:43 +0100

    hello world

--- index.php
+++ index.php
@@ -15,9 +15,27 @@
             //$myname = "Cameron Lawrie";
             //$myage = 21;
             //echo "My name is " .  $myname . " and I am " . $myage;
-            $name = "Simon";
-            if ($name == "Simon") {     print "I know you!";
-            }   else {     print "Who are you?";   }
+
+            //$name = "Simon";
+           // if ($name == "Simon") {     print "I know you!";
+           // }   else {     print "Who are you?";   }
+
+            $wantedgood = "Mugs";
+
+
+            switch ($wantedgood) {
+                case "mugs":
+                    echo "You have to be 13 to buy mugs";
+                    break;
+                case "specs":
+                    echo "You have to be 15 to buy specs";
+                    break;
+                case "sausage rolls":
+                    echo "You have to be 18 to buy sausage rolls";
+                    break;
+                default:
+                    echo "Invalid entry, please enter mugs, specs or sausage rolls";
+            }
             ?>
         </p>
     </body>
