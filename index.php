a7c6959663317668703921c570e66364f126cf8e
1301118 <1301118@rgu.ac.uk>
2016-10-03 13:50:49 +0100

    hello world

---
+++ index.php
@@ -0,0 +1,19 @@
+/**
+ * Created by PhpStorm.
+ * User: 1301118
+ * Date: 03/10/2016
+ * Time: 13:42
+ */
+
+<!DOCTYPE html>
+<html>
+<head>
+    </head>
+    <body>
+        <p>
+            <?php
+            echo "hello world";
+            ?>
+        </p>
+    </body>
+</html>
