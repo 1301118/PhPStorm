711a1cfedc78ed18d0198d160997fde4bcd53f30
1301118 <1301118@rgu.ac.uk>
2016-10-10 15:42:24 +0100

    hello world

--- Week2.1/allMovies.php
+++ Week2.1/allMovies.php
@@ -10,5 +10,9 @@ include("DBCONNECT.php");
 
 $sql_query = "SELECT * FROM marvelmovies";
 
+$result = mysqli_query($db, $sql);
+
+$row = mysql_fetch_array($result, MYSQLI_ASSOC);
+
 ?>
 
---
+++ Week2.1/xmenFilms.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1301118
+ * Date: 10/10/2016
+ * Time: 15:39
+ */
+
+include("DBCONNECT.php");
+
