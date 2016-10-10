516381e814145eec709702b812cc4d3ff748e26f
1301118 <1301118@rgu.ac.uk>
2016-10-10 14:39:21 +0100

    hello world

--- Week2/Database.php
+++ Week2/Database.php
@@ -5,3 +5,15 @@
  * Date: 10/10/2016
  * Time: 13:54
  */
+
+
+$db = eu-cdbr-azure-north-e.cloudapp.net(
+       'eu-cdbr-azure-north-e.cloudapp.net',
+        'b8de00c1673432',
+        'da12052a',
+        'cameronlawrie'
+    );
+
+if ($db->connect_error){
+    die('connectionfailed ['.$db->connect_error.' ]');
+}
