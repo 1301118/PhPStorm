424aa89ccaa51a14c229a678ba123f58865c3d07
1301118 <1301118@rgu.ac.uk>
2016-10-17 14:32:35 +0100

    hello world

--- Week3/Activity2/formResponse.php
+++ Week3/Activity2/formResponse.php
@@ -8,5 +8,11 @@
 
 $forname = $_POST["forename"];
 $surname = $_POST["surname"];
+$gender = $_POST["gender"];
+$power = $_POST["power"];
+
+echo "Hello {$forname}{$surname}. You have specififed your super power as {$power}. You have specififed your gender a {$gender}"
+
+
 
 ?>
