8f0187cdc6e82083727c4f4ee5c3b35a67c4112d
1301118 <1301118@rgu.ac.uk>
2016-10-31 14:20:56 +0000

    hello world

--- Week6/inc/contactus.php
+++ Week6/inc/contactus.php
@@ -3,8 +3,8 @@ include ("scripts/header.php");
 echo "
 <main>
 <h2>Contact</h2>
-<p>Send me an email at babbage@babbage-labs.com</p>
+<p>Send me an email at cammy@RGU-labs.com</p>
 </main>
 ";
 include ("scripts/footer.php");
-?> 
+?>
