01e70eeee314c9dfe6830be08c4d2ed7f7e2d71a
1301118 <1301118@rgu.ac.uk>
2017-01-09 13:17:47 +0000

    rev

--- Revision/Rev.php
+++ Revision/Rev.php
@@ -1,5 +1,12 @@
 <?php
 
+$juice = 'apple';
+
+echo "I drank some $juice juice<br />";
+echo 'I drank some $juice juice<br />';
+echo "i drank some $juice";
+echo 'I drank some $juice';
+
 $x = "Hello world!";
 $y = 'Hello world!';
 
